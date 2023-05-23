a779177e072cef60b973535482d91bec5a345585
David Dieruf <david@dierufventures.com>
2023-05-23 10:17:18 -0400

    Remove all the generated js

--- src/types/TTopic.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TopicType = void 0;
-var TopicType;
-(function (TopicType) {
-    TopicType["Persistent"] = "Persistent";
-    TopicType["NonPersistent"] = "NonPersistent";
-})(TopicType = exports.TopicType || (exports.TopicType = {}));
